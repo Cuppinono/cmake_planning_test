0e93a09a7069fe2cb1cc2d240ed0c2c77f899ca1
Cuppinono <1551434506@qq.com>
2024-04-23 01:24:13 +0800

    0.0.1

---
+++ pnc_map.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "pnc_map.h"
+
+void Pnc_Map::map_info()
+{
+    std::cout << "this is pnc_map" << std::endl;
+}
---
+++ process.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "pnc_map.h"
+
+class Process
+{
+public:
+    void plan_process();
+
+public:
+    Pnc_Map my_map;
+};
